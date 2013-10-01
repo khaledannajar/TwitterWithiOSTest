9b4d86bdabfb993b5ea59b8db1c3345d1440b458
khalid_annajar <khalid_annajar@siliconexpert.com>
2013-10-01 11:52:02 +0200

    initial commit

---
+++ twitter-feeds-app/twitter-feeds-app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twitter-feeds-app
+//
+//  Created by SE on 10/1/13.
+//  Copyright (c) 2013 khaled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twitter-feeds-app/twitter-feeds-app/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  twitter-feeds-app
+//
+//  Created by SE on 10/1/13.
+//  Copyright (c) 2013 khaled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
