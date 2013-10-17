1b5a6001ddf3b349788e8fd186cd3ff0db174941
Khaled Annajar <khaledannajar@gmail.com>
2013-10-17 23:46:47 +0200

    fix call to wrong name of Model.

--- twitter-feeds/DataManager.m
+++ twitter-feeds/DataManager.m
@@ -52,7 +52,7 @@
     if (_managedObjectModel != nil) {
         return _managedObjectModel;
     }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Tweet" withExtension:@"momd"];
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"TweetModel" withExtension:@"momd"];
     _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     return _managedObjectModel;
 }
